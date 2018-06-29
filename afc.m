ea0577f6de6f90346244f424370a9403caf26966
Gidon Ersnt <gidon.ernst@unimelb.edu.au>
2018-06-29 15:05:06 +1000

    import S-Taliro model

--- afc.m
+++ afc.m
@@ -1,39 +1,14 @@
 addpath 'src/test/simulink/'
-InitBreach
+
+simTime = 50.0
+en_speed = 1000.0
+measureTime = 1.0
+fault_time = 60.0
+spec_num = 1.0
 fuel_inj_tol = 1.0
 MAF_sensor_tol = 1.0
 AF_sensor_tol = 1.0
-pump_tol = 1.0
-kappa_tol = 1.0
-tau_ww_tol = 1.0
-fault_time = 60.0
-kp = 0.04
-ki = 0.14
-sys = BreachSimulinkSystem('fuel_control')
-sys.Sys.tspan = 0:50.0
-phi = STL_Formula('(alw_[11.0,50.0] ((((throttle[t] < 8.8) and (ev_[0.0,0.05] (40.0 < throttle[t]))) or ((40.0 < throttle[t]) and (ev_[0.0,0.05] (throttle[t] < 8.8)))) => (alw_[1.0,5.0] (abs(mu[t]) < 0.008))))', '(alw_[11.0,50.0] ((((throttle[t] < 8.8) and (ev_[0.0,0.05] (40.0 < throttle[t]))) or ((40.0 < throttle[t]) and (ev_[0.0,0.05] (throttle[t] < 8.8)))) => (alw_[1.0,5.0] (abs(mu[t]) < 0.008))))')
-gen_throttle = fixed_cp_signal_gen({'throttle'}, 10)
-gen_engine = constant_signal_gen({'engine'})
-gen = BreachSignalGen({gen_throttle, gen_engine})
-sys.SetInputGen(gen)
-sys.SetParamRanges({'throttle_u0'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u1'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u2'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u3'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u4'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u5'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u6'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u7'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u8'}, [0.0 61.1])
-sys.SetParamRanges({'throttle_u9'}, [0.0 61.1])
-sys.SetParamRanges({'engine_u0'}, [900.0 1100.0])
-problem = FalsificationProblem(sys, phi)
-problem.max_obj_eval = 100
-problem.max_time = 1000
-problem.setup_solver('cmaes')
-problem.solver_options.Seed = 0
-problem.solve()
-time = problem.time_spent
-sims = problem.nb_obj_eval
-score = problem.obj_best
-best = problem.BrSet_Best
+
+t__ = [0.0; 5.0; 10.0; 15.0; 20.0; 25.0; 30.0; 35.0; 40.0; 45.0; 50.0]
+u__ = [16.799687324768122 1088.2498358964228; 8.957361282202264 1088.2498358964228; 33.40579916482625 1088.2498358964228; 6.384380929934371 1088.2498358964228; 25.09964754450387 1088.2498358964228; 60.53316220717324 1088.2498358964228; 45.59534986519439 1088.2498358964228; 49.93685106310972 1088.2498358964228; 32.181335454357324 1088.2498358964228; 8.183724259859115 1088.2498358964228; 8.183724259859115 1088.2498358964228]
+result = sim('AbstractFuelControl_M1', 'StopTime', '50.0')
